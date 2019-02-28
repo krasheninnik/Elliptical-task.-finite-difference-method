4d59d9eb6f704c6e7b41ca448c5a793008967967
krasheninnik <krasheninnik99@gmail.com>
2019-03-01 00:26:08 +0700

    changed uneven partition grid

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "EllipticalTask.h"
+#include "setting.h"
+#include <iostream>
+
+int main()
+{
+	EllipticalTask T;
+	
+#ifdef EXPLORE_CONVERGENCE
+	const int amount_iters = 7;
+	for(int i = 0; i < amount_iters; i++) {
+		std::cout << "\riteration " << i << " of " << amount_iters << std::endl;
+		T.init(i);
+
+#ifdef CALCULATE
+		T.solve(true);	// solve task
+#else
+		T.test();		// check the correctness of the decision on the given functions
+#endif
+	}
+
+#else
+	T.init(0);
+#ifdef CALCULATE
+	T.solve(true);	// solve task
+#else
+	T.test();		// check the correctness of the decision on the given functions
+#endif
+#endif
+	
+}
